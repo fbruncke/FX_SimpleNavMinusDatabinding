57d66561dd1f9102d354fe680469d304ebe459e6
Frank <fha@easv.dk>
2024-03-19 18:36:39 +0100

    Initial commit

---
+++ src/main/java/com/example/fx_simplenavminusdatabinding/EmptyviewController.java
@@ -0,0 +1,26 @@
+package com.example.fx_simplenavminusdatabinding;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class EmptyviewController {
+    @FXML
+    private Label welcomeText;
+
+    @FXML
+    protected void onHelloButtonClick() {
+        welcomeText.setText("Welcome emptyview controller!");
+    }
+
+
+
+
+    public void changeView(ActionEvent actionEvent)
+    {
+        //welcomeText.setText("Test");
+        //Stage primaryStage = (Stage)((Node)actionEvent.getSource()).getScene().getWindow();
+        Main.changeScene(ControllerNames.DetailsView);
+
+    }
+}
